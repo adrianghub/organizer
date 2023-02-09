7688de994ec29a9c0c7b6fcc0e9f623996bf71aa
Adrian Zinko <adrian.zinko@boldare.com>
2023-02-09 22:01:38 +0100

    init: setup project config & init db scheme migration

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Organizer</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Project organizer" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
